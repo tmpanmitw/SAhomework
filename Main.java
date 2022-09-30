f91a466205f15eeb898a68a7796685f61b97b74c
tmpanmitw <112293743+tmpanmitw@users.noreply.github.com>
2022-10-01 02:53:31 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
+        ///////////////////////////////////////////버스 시나리오////////////////////////////////////////////////
         bus bus1 = new bus(1);
         bus bus2 = new bus(2);
 
@@ -25,7 +26,32 @@ public class Main {
         bus1.changeForm_bus();
         System.out.println("상태 = "+bus1.bus_form);
 
+        ///////////////////////////////////////////택시 시나리오////////////////////////////////////////////////
 
+        taxi taxi1 = new taxi(1);
+        taxi taxi2 = new taxi(2);
+
+        System.out.println("주유량 = "+taxi1.fuelVolume);
+        System.out.println("상태 = "+taxi1.taxi_form);
+
+        taxi1.onboarding_taxi(2, "서울역",2);
+
+        taxi1.fuelVolume -= 80;
+        System.out.println("주유량 = "+taxi1.fuelVolume);
+        taxi1.payment();
+
+        System.out.println(taxi1.nowPassenger);
+        taxi1.onboarding_taxi(5,"구로디지털단지역", 12);
+        taxi1.onboarding_taxi(3,"구로디지털단지역", 12);
+
+        taxi1.fuelVolume -= 20;
+        taxi1.payment();
+
+        System.out.println("주유량 = "+taxi1.fuelVolume);
+        System.out.println("상태 = "+taxi1.taxi_form);
+        System.out.println("누적 요금 = "+ taxi1.money_total);
+        taxi1.changeForm_taxi();
+        System.out.println("상태 = "+taxi1.taxi_form);
 
 
     }
--- taxi.java
+++ taxi.java
@@ -1,13 +1,16 @@
 /////////////////////////////////////////////TAXI 모델링/////////////////////////////////////////////////////////
 
 class taxi extends Transportation{
-    int bus_number;
+    int taxi_number;
     String destination;
     int distanceToDestination;
     int base_distance;
     int rate_per_Distance;
     String taxi_form;           //운행중, 일반, 운행불가
     int rate_total;
+    int nowPassenger;
+    int money_total;
+
 
     void start_driving(){
 
@@ -15,13 +18,26 @@ class taxi extends Transportation{
     void changeForm_taxi(){
         if(fuelVolume>=10){
             taxi_form = "운행";
+        }else{
+            taxi_form = "운행불가";
+            System.out.println("주유 필요");
         }
     }
-    void onboarding_taxi(int passenger){
-        if ( taxi_form == "일반"){
-            taxi_form = "운행중";
-        }else{
-            System.out.println("탑승불가");
+    void onboarding_taxi(int passenger, String destination, int distanceToDestination){
+        if ( taxi_form == "일반") {
+            if (nowPassenger <= maxPassenger) {
+                taxi_form = "운행중";
+                nowPassenger += passenger;
+                System.out.println("탑승 승객 수 = " + nowPassenger);
+                System.out.println("잔여 승객 수 = " + (maxPassenger - nowPassenger));
+                System.out.println("기본 요금 확인 = " + base_rate);
+                System.out.println("목적지 = " + destination);
+                System.out.println("목적지까지 거리 = " + distanceToDestination + "km");
+                add_rate(distanceToDestination);
+                System.out.println("상태 = " + taxi_form);
+            }else{
+                System.out.println("최대 승객 수 초과");
+        }
         }
     }
     void changespeed_taxi(int changespeed){
@@ -30,16 +46,22 @@ class taxi extends Transportation{
 
     void add_rate(int distanceToDestination){
         if(distanceToDestination>base_distance){
-            rate_total = base_rate + (distanceToDestination-base_distance)/rate_per_Distance;
+            rate_total = base_rate + rate_per_Distance*(distanceToDestination-base_distance);
+            System.out.println("지불할 요금 = "+rate_total);
         }else{
             rate_total = base_rate;
+            System.out.println("지불할 요금 = "+rate_total);
         }
     }
     void payment(){
-        System.out.println("최종요금 : "+rate_total );
+        System.out.println("누적 요금 = "+rate_total );
+        nowPassenger = 0;
+        taxi_form = "일반";
+        money_total +=rate_total;
     }
-    taxi(){
+    taxi(int taxi_number){
         super(100, 0);
+        this.nowPassenger = 0;
         this.maxPassenger = 4;
         this.base_rate = 3000;
         this.rate_per_Distance = 1000;
@@ -47,6 +69,8 @@ class taxi extends Transportation{
         this.fuelVolume = 100;
         this.taxi_form = "일반";
         this.speed = 0;
+        this.money_total = 0;
+        System.out.println(taxi_number+"번 택시 생성");
 
     }
 }
